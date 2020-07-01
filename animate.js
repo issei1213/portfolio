c789a8ceccfa186e230eca7f1c4aa7581573f24d
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-07-01 18:04:07 +0900

    updated

--- animate.js
+++ animate.js
@@ -1,15 +1,12 @@
 $(function(){
   // #で始まるリンクをクリックしたら実行されます
-  $('a[href^="#header-top"]').click(function() {
+  $('a[href^="#"]').click(function() {
     // スクロールの速度
     var speed = 400; // ミリ秒で記述
     var href= $(this).attr("href");
-    console.log(`href:${href}`)
     var target = $(href == "#" || href == "" ? 'html' : href);
-    console.log(`target:${target}`)
     var position = target.offset().top;
-    console.log(`position:${position}`)
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    $("body,html").animate({scrollTop:position}, speed, 'swing');
     return false;
   });
 });
